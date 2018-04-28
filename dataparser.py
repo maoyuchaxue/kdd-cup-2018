d9055171bb9bebed29d11bd1895601b2a97b1739
junxian shen <sjx15@mails.tsinghua.edu.cn>
2018-04-28 13:07:33 +0800

    add beijing aqstation position parser and data

---
+++ dataparser.py
@@ -0,0 +1,17 @@
+import csv
+
+## [aqstation, longitude, altitude]
+def getAqStation(city):
+    aq_station_list = []
+    if city == "beijing":
+        fn = "data/beijing_aq_station.csv"
+    csv_file = csv.reader(open(fn, 'r'))
+    for line in csv_file:
+        if len(line) > 0:
+            aq_station_list.append([line[0], float(line[1]), float(line[2])])
+    return aq_station_list
+
+
+aq_station =  getAqStation("beijing")
+for station in aq_station:
+    print station
